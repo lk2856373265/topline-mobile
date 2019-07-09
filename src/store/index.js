c2565d95161d029389a98ef379dfd7ea82c30a78
linkun <2856373265@qq.com>
2019-07-09 11:33:13 +0800

    '调整目录结构'

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+// 1.npm install vuex
+
+// 2.配置
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+// 3.创建容器实例
+const store = new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  }
+})
+export default store

37adce6afebd105901f11584cdc0f01b543c3519
linkun <2856373265@qq.com>
2019-07-10 16:04:54 +0800

    '适配rem及基于axios封装request请求模块'

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,26 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-    'postcss-pxtorem': {
-      rootValue: 37.5,
-      propList: ['*']
+// module.exports = {
+//   plugins: {
+//     autoprefixer: {},
+//     'postcss-pxtorem': {
+//       rootValue: 37.5,
+//       propList: ['*']
+//     }
+//   }
+// }
+const { sep } = require('path')
+
+module.exports = ({ file }) => {
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
     }
   }
 }

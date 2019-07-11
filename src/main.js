c9b232cf7e7fb64b8d97a449a3a7c7ee81f1728a
linkun <2856373265@qq.com>
2019-07-11 21:09:59 +0800

    '登录成功存储用户token到容器中'

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import Vant from 'vant'
+import store from './store'
 import 'vant/lib/index.css'
 import 'amfe-flexible'
 import './styles/index.less'
+
 Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: function (h) { return h(App) }
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,25 @@
 // 2.配置
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as auth from '@/utils/auth'
+// import { saveUser } from '../utils/auth';
 
 Vue.use(Vuex)
 // 3.创建容器实例
-const store = new Vuex.Store({
+export default new Vuex.Store({
   state: {
+    // 初始化的时候从本地存储获取数据，防止刷新丢失token
+    user: auth.getUser()
   },
   mutations: {
+    /**
+     * 登录成功，调用mutation更新容器中的user状态
+     */
+    setUser (state, data) {
+      state.user = data
+      auth.saveUser(state.user)
+    }
   },
   actions: {
   }
 })
-export default store

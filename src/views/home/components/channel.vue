6b2282de36c4130dc91923c5bfdfa8fc301d7d69
linkun <2856373265@qq.com>
2019-07-17 11:24:23 +0800

    '频道管理-不允许删除固定频道'

--- src/views/home/components/channel.vue
+++ src/views/home/components/channel.vue
@@ -34,7 +34,7 @@
         class="text"
         :class="{ active: index === activeIndex && !isEdit}"
         >{{ item.name}}</span>
-        <van-icon class="close-icon" v-show="isEdit" name="close" />
+        <van-icon class="close-icon" v-show="isEdit && !aliveChannels.includes(item.name)" name="close" />
         </van-grid-item>
       </van-grid>
     </div>
@@ -90,7 +90,8 @@ export default {
   data () {
     return {
       allChannels: [], // 所有频道列表
-      isEdit: false
+      isEdit: false,
+      aliveChannels: ['推荐', 'css']
     }
   },
   computed: {
@@ -162,7 +163,7 @@ export default {
         this.changeChannel(item, index)
       } else {
         // 编辑状态：删除频道
-        this.deleteChannel(item, index)
+        !this.aliveChannels.includes(item.name) && this.deleteChannel(item, index)
       }
     }
   }

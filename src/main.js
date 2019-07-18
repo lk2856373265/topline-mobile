3d32565c4aa924edb3901e32efc1018331332b65
linkun <2856373265@qq.com>
2019-07-18 09:25:15 +0800

    '文章列表-配置dayjs日期处理库'

--- src/main.js
+++ src/main.js
@@ -9,13 +9,21 @@ import 'vant/lib/index.css'
 import 'amfe-flexible'
 import './styles/index.less'
 
-Vue.use(Lazyload)
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn' // 加载中文语言包
+import relativeTime from 'dayjs/plugin/relativeTime' // dayjs的相对时间插件
+
+dayjs.extend(relativeTime) // 把插件注册到dayjs中
+dayjs.locale('zh-cn') // 配置使用中文语言包
+console.log(dayjs().from(dayjs('2018-12-01')))
 Vue.use(VeeValidate, {
   // 配置改变的时候触发校验，默认是input
   events: ''
 })
 Validator.localize('zh_CN', zhCN)
+
 Vue.use(Vant)
+Vue.use(Lazyload)
 
 Vue.config.productionTip = false
 Vue.prototype.$sleep = time => {

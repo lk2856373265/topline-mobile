7ad661724c3affcae3aeda77fdf327c51c333a98
linkun <2856373265@qq.com>
2019-07-20 20:04:39 +0800

    '搜索结果-处理页面结构'

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -1,16 +1,64 @@
 <template>
-  <div>搜索结果页面{{ $route.params.q }}</div>
+  <div>
+    <!-- 导航头部 -->
+    <van-nav-bar
+      title="搜索结果"
+      left-text="返回"
+      fixed
+      left-arrow
+      @click-left="$router.back()"
+    />
+     <!-- /导航头部 -->
+     <!-- 文章列表 -->
+     <van-list
+        class="article-list"
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
+        <van-cell
+          v-for="item in list"
+          :key="item"
+          :title="item"
+        />
+      </van-list>
+     <!-- /文章列表 -->
+  </div>
 </template>
 
 <script>
 export default {
   name: 'SearchResult',
   data () {
-    return {}
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
   },
-  components: {}
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.article-list {
+  margin-top: 92px
+}
 </style>

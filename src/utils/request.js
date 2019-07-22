0fa3ee54cb125b35197f0bff730f0209fe760d5b
linkun <2856373265@qq.com>
2019-07-22 11:00:00 +0800

    '实现token过期刷新token功能'

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import store from '@/store'
 import JSONbig from 'json-bigint'
+import router from '@/router'
 /**
  * axios.create 用于创建一个 axios 实例，该实例和 axios 的功能是一模一样的
  * 说白了就是克隆了一个 axios
@@ -29,7 +30,7 @@ request.interceptors.request.use(function (config) {
   const { user } = store.state
   // 如果已登录，则为请求接口统一添加用户token
   if (user) {
-    config.headers.Authorization = `Bearer ${user.token} `
+    config.headers.Authorization = `Bearer ${user.token}`
   }
   return config
 }, function (error) {
@@ -44,7 +45,42 @@ request.interceptors.response.use(function (response) {
   // 如果响应结果对象中有 data，则直接返回这个 data 数据
   // 如果响应结果对象中没有 data，则不作任何处理，直接原样返回这个数据
   return response.data.data || response.data
-}, function (error) {
+}, async error => {
+  if (error.response.status === 401) {
+    // 看一下有没有 user （有user 就有refresh_token）
+    const user = store.state.user
+    // 如果没有user ,则跳转到登录页去登录
+    if (!user) {
+      router.push({
+        name: 'login'
+      })
+      return
+    }
+    // 如果有user ,请求接口使用refresh_token 获取新的访问token
+    try {
+      // 注意：请求刷新token 不要使用request
+      const { data } = await axios({
+        method: 'PUT',
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${user.refresh_token}`
+        }
+      })
+      // 拿到新的登录令牌
+      // 更新用户token
+      store.commit('setUser', {
+        token: data.data.token,
+        refresh_token: user.refresh_token
+      })
+      // 把本次因为token过期而发送失败的请求继续发出去
+      return request(error.config)
+    } catch (refreshErr) {
+      // 刷新token 请求失败，直接去登录页
+      router.push({
+        name: 'login'
+      })
+    }
+  }
   // Do something with response error
   return Promise.reject(error)
 })
